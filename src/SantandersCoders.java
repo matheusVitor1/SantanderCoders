e58fb62841ce16307a0fb4f3c2c05b7f81384f75
matheusVitor1 <matheus_vitoralves@hotmail.com>
2023-07-12 22:41:32 -0300

    Initial commit

---
+++ src/SantandersCoders.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SantandersCoders {
+    /* Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+       C = 5 * ((F-32) / 9).
+
+    */
+
+
+    public static Double converteTemperatura(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a temperatura em graus Fahrenheit");
+        double fahrenheit = sc.nextDouble();
+
+        double celsius = 5 * ((fahrenheit - 32) / 9);
+        sc.close();
+
+        return celsius;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        double temperaturaCelsius = converteTemperatura();
+        System.out.printf("A temperatura em graus Celsius é: %.2f%n", temperaturaCelsius);
+    }
+}
